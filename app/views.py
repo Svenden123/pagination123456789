17ce76dafa02c3231ae0e1c712e4d46639845d5d
Герман <buhrus2@yandex.ru>
2019-06-13 18:54:26 +0400

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render_to_response, redirect
+from django.urls import reverse
+from csv import DictReader
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.conf import settings
+
+
+def index(request):
+    return redirect(reverse(bus_stations))
+
+
+def bus_stations(request):
+    with open(settings.BUS_STATION_CSV, encoding='cp1251') as f:
+        csv_data = DictReader(f)
+        stations = []
+        for station in csv_data:
+            stations.append(
+                {
+                    'Name': station['Name'],
+                    'Street': station['Street'],
+                    'District': station['District']
+                }
+            )
+    paginator = Paginator(stations, 10)
+    page = request.GET.get('page')
+
+    try:
+        get_station = paginator.page(page)
+    except PageNotAnInteger:
+        get_station = paginator.page(1)
+    except EmptyPage:
+        get_station = paginator.page(paginator.num_pages)
+
+    if get_station.number == 1:
+        prev_page_url = None
+    else:
+        prev_page_url = f'?page={get_station.previous_page_number()}'
+
+    next_page_url = f'?page={get_station.next_page_number()}'
+
+    return render_to_response('index.html', context={
+        'bus_stations': get_station,
+        'current_page': get_station.number,
+        'prev_page_url': prev_page_url,
+        'next_page_url': next_page_url,
+    })
+
